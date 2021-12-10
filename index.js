fbf26ae9c1150c1e1e81ed1100d453af4a418b44
Mark <mark.j.wee@gmail.com>
2021-12-10 15:07:22 -0500

    commit tree f5cff517910e3f82afdbda7a16702c543ca3a752

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(funActivity="roller-skate") { {
+    return  `This Saturday, I want to ${funActivity}!`
+    }
+}
+
+function mondayWork(workActivity="go to the office") { {
+    return `This Monday, I will ${workActivity}.`
+    }
+}
+
+function wrapAdjective(highlight) { {
+    return function wrapMiddleStep(adjectivePhrase){
+        return `You are ${highlight}${adjectivePhrase}${highlight}!`
+        }
+    }
+}
